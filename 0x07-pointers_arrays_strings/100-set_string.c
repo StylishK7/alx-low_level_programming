c2a97c92d78f9903776cbc94ac68c29846e63554
StylishK7 <codexorg7@gmail.com>
2023-09-26 04:15:51 -0700

    All 0x07-pointers_arrays_strings

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+   * set_string - Entry Point
+    * @s: input
+     * @to: input
+      * Return: Always 0
+      */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
