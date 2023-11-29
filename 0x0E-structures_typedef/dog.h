6589bf48893ef035bd8313cfe7c4f3dcf57417f0
Stylish <codexorg7@gmail.com>
2023-11-28 16:23:30 -0800

    All preprocessor

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_HEADER_FILE
+#define DOG_HEADER_FILE
+/*
+* struct dog - contains elements of dog.
+*/
+
+struct dog
+{
+char * name;
+double age;
+char * owner;
+};
+
+typedef struct dog dog_t;
+
+#endif
